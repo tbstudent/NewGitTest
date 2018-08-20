05dee3b263d8b5da5e5d0ff0f3c50e6345ebc666
Biyik <S02204085@ppcc.ccofc.edu>
2018-08-20 16:59:24 -0600

    Some for loops and the addition of the while loop included in the guessing game which is a plusgit add -A

--- practice.js
+++ practice.js
@@ -28,18 +28,51 @@
 
 // This next while loop will demostrate the use of while loops to get the user to pin point the number the website is thinking of.
 
-var number=14;
-guess= Number(prompt("Can You guess the number Im thinking about"));
-while(guess!==14){
-	if (guess<14){
-		guess= Number(prompt("Too low try again"));
-	}
-	else if(guess>14){
-		guess= Number(prompt("Too high try again"));
-	}
+// var number=14;
+// guess= Number(prompt("Can You guess the number Im thinking about"));
+// while(guess!==14){
+// 	if (guess<14){
+// 		guess= Number(prompt("Toooo low try again"));
+// 	}
+// 	else if(guess>14){
+// 		guess= Number(prompt("Too high try again"));
+// 	}
 	
 	
 
+// }
+
+// 		alert("Awesome you got it right!");
+
+// this next thing will annoy you till you "By repeating are we there yet?" type the words yes or yeah than it will
+// alert Yayy we made it! V1
+// var answer = prompt("Are we there yet");
+
+// while (answer !== "yes" && answer !== "yeah"){
+// 	var answer = prompt("Are we there yet");
+// }
+// alert("YaAAYY we made it");
+
+//This version of this annoy-o-matic asks the same question over and over again until the answer you input 
+// At least has a "yeah" or "yes" in it.
+
+// var answer = prompt("Are we there yet");
+
+// while (answer.indexOf("yes")===-1 && answer.indexOf("yeah")===-1){
+// 	var answer = prompt("Are we there yet");
+// }
+// alert("YaAAYY we made it");
+
+// This next is an example of a forloop printing all numbers between -10-19
+
+// for(var num=-10; num<20; num++){
+// 	console.log(num);
+// }
+//This next is an example of a forloop printing all even numbers between 10-40
+
+for(var num=10; num<=40 ; num+=1){
+	if(num % 2  === 0){
+	console.log(num);
 }
 
-		alert("Awesome you got it right!");
+}
